6a30e0e0f7066c067aac8667f51904bb74451978
Lisa Corcoran <lisa.corcoran@booking.com>
2016-09-17 14:09:17 +0200

    add Box2D plugin

--- README.md
+++ README.md
@@ -3,10 +3,8 @@ simple game using phaser
 
 http:phaser.io
 
-So far we've used the particle plugin, weapon plugin and gravity via the arcade physic engine.
-And the phaser-HudManager plugin https://github.com/codevinsky/phaser-HudManager from codevinsky https://github.com/codevinsky
-We've added a oil pump animation, consisting of 3 parts
+We've added a oil pump animation, consisting of 3 parts, using Box2D Engine
 
-Stage1 - Stage2 - Stage3 - Stage4 - Stage5 - Stage6 - Stage7
+Stage1 - Stage2 - Stage3 - Stage4 - Stage5 - Stage6 - Stage7 - Stage8 - Stage9
 
-![Platform Game - Stage 7](https://saturdaycodersamsterdam.github.io/Stage7.png)
+![Platform Game - Stage 9](https://saturdaycodersamsterdam.github.io/Stage9.png)
--- script.js
+++ script.js
@@ -56,7 +56,6 @@ Smc.playerTypes.Player = function (name, phaserObject) {
     //  But the 'true' argument tells the weapon to track sprite rotation
     this._weaponPhaserObject.trackSprite(this._weaponMountPhaserObject, 0, 0, true);
 
-
     this._id                  = 1;
     this._phaserObject.maxHealth = 100;
     this._phaserObject.health= 100;
@@ -203,7 +202,7 @@ function buildPhaserEventHandler(eventName) {
     // passed on to this builder function.
     return function() {
         Smc.phaserEventHandlers[eventName].forEach(function (handler) {
-            handler();
+            handler('hello');
         });
     }
 }
@@ -219,20 +218,10 @@ var liftY = 250;
 var lift ;
 var mexican;
 
-var armX = 46;
-var armY= 93;
-var pumpX = 62;
-var pumpY= 168;
-var weightX = 0;
-var weightY= 345;
-
 var x = game.width/2;
 var y = game.height/2;
 
 Smc.phaserEventHandlers.preload.push(function() {
-    game.load.image('arm', 'assets/arm.png');
-    game.load.image('pump', 'assets/pump.png');
-    game.load.image('weight', 'assets/weight.png');
 
     game.load.image('fire1', 'assets/fire1.png');
     game.load.image('fire2', 'assets/fire2.png');
@@ -250,8 +239,15 @@ Smc.phaserEventHandlers.preload.push(function() {
 
     game.load.image('pixel', 'assets/trans-pixel.png');
     game.load.script('HudManager', 'plugins/HUDManager.js');
-});
 
+    game.load.image('arm', 'assets/arm.png');
+    game.load.image('pump', 'assets/pump.png');
+    game.load.image('weight', 'assets/weight.png');
+
+});
+Smc.phaserEventHandlers.preload.push(function(arg) {
+    console.log("this works",arg);
+});
 Smc.phaserEventHandlers.create.push(function() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.stage.backgroundColor = '#333';
@@ -263,17 +259,59 @@ Smc.phaserEventHandlers.create.push(function() {
     box = game.add.sprite( boxX, boxY, 'box');
     lift = game.add.sprite( liftX, liftY, 'lift');
 
-
-    pump = game.add.sprite( pumpX, pumpY, 'pump');
-    arm = game.add.sprite( armX, armY, 'arm');
-    weight = game.add.sprite( weightX, weightY, 'weight');
-
     game.physics.arcade.enable(box);
     game.physics.arcade.enable(lift);
     lift.body.collideWorldBounds = true;
     box.body.collideWorldBounds = true;
 
     cursors = game.input.keyboard.createCursorKeys();
+
+    // Enable Box2D physics
+    game.physics.startSystem(Phaser.Physics.BOX2D);
+    
+    game.physics.box2d.debugDraw.joints = true;
+    game.physics.box2d.gravity.y = 500;
+    game.physics.box2d.restitution = 0.7;
+
+    game.physics.box2d.setBoundsToWorld();
+
+    //  Create a static rectangle body for the ground. This gives us something solid to attach our crank to
+    var ground = new Phaser.Physics.Box2D.Body(this.game, null, game.world.centerX, 470, 0);
+    //setRectangle(width, height, offsetX, offsetY, rotation)
+    ground.setRectangle(640, 20, 0, 0, 0);
+
+    //  Tall skinny rectangle body for the crank
+    var crank = game.add.sprite( game.world.centerX, 310, 'weight');
+    crank.anchor.setTo(0.5, 0.5);
+    game.physics.box2d.enable(crank);
+    crank.body.setCircle(crank.width / 2);
+    //Revolute joint with motor enabled attaching the crank to the ground. This is where all the power for the slider crank comes from
+    game.physics.box2d.revoluteJoint(ground, crank, 0, -80, 0, 0, 250, 50, true);
+    
+    //  Tall skinny rectangle body for the arm. Connects the crank to the piston
+    var arm = game.add.sprite( game.world.centerX, game.world.centerY, 'pump');
+    game.physics.box2d.enable(arm);
+    arm.body.setRectangle(10, 179, 0, 0, 0);
+    //arm.anchor.setTo(0, 0.5);
+    //revolute joint to attach the crank to the arm
+    game.physics.box2d.revoluteJoint(crank, arm, 0, -30, 0, 60);
+    
+    //  Square body for the piston. This will be pushed up and down by the crank
+    var piston = game.add.sprite( 0, 310, 'arm');
+    game.physics.box2d.enable(piston);
+    piston.body.setRectangle(301, 112, 150, 0, 0);
+    piston.anchor.setTo(0, 0.5);
+    //revolute joint to join the arm and the piston
+    // bodyA, bodyB, ax, ay, bx, by, motorSpeed, motorTorque, motorEnabled, lowerLimit, upperLimit, limitEnabled
+    game.physics.box2d.revoluteJoint(arm, piston, 0, -112, 0, 0);
+    //prismatic joint between the piston and the ground, this joints purpose is just to restrict the piston from moving on the x axis
+    game.physics.box2d.prismaticJoint(ground, piston, 0, 1, 0, 0, 0, 0);
+
+    // Set up handlers for mouse events
+    game.input.onDown.add(mouseDragStart, this);
+    game.input.addMoveCallback(mouseDragMove, this);
+    game.input.onUp.add(mouseDragEnd, this);
+
 });
 
 Smc.phaserEventHandlers.update.push(function() {
@@ -284,7 +322,6 @@ Smc.phaserEventHandlers.update.push(function() {
         mexican.hit();
         bulletPhaserObject.kill();
     }, null, this);
-
 
     if (cursors.up.isDown) {
         student.moveUp();
@@ -298,9 +335,13 @@ Smc.phaserEventHandlers.update.push(function() {
     if (cursors.right.isDown) {
         student.moveRight();
     }
-
     if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
         student.fireWeapon();
     }
 
 });
+
+
+function mouseDragStart() { game.physics.box2d.mouseDragStart(game.input.mousePointer); }
+function mouseDragMove() { game.physics.box2d.mouseDragMove(game.input.mousePointer); }
+function mouseDragEnd() { game.physics.box2d.mouseDragEnd(); }
